2c321e6e791cd5476501945e70adbb9235326bc5
Baptiste Pachot <baptiste.pachot@telecomnancy.eu>
2025-01-06 10:40:14 +0100

    ajout fichier grid

--- src/main/java/linguacrypt/model/Grid.java
+++ src/main/java/linguacrypt/model/Grid.java
@@ -1,4 +1,27 @@
 package linguacrypt.model;
 
 public class Grid {
+    private int size;
+    private Card[][] grid;
+
+    public Grid(int size) {
+        this.size = size;
+        this.grid = new Card[size][size];
+    }
+
+    public Card[][] getGrid(){
+        return grid;
+    }
+    public Card getCard(int row, int col) {
+        if (row >= 0 && row < size && col >= 0 && col < size) {
+            return grid[row][col];
+        }
+        return null;
+    }
+
+    public void setCard(int row, int col, Card card) {
+        if (row >= 0 && row < size && col >= 0 && col < size) {
+            grid[row][col] = card;
+        }
+    }
 }
