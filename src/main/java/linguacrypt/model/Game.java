de4476502cbf773e6172dca40147df5c0675ddff
Baptiste Pachot <baptiste.pachot@telecomnancy.eu>
2025-01-06 10:48:49 +0100

    ajout fichier game

--- src/main/java/linguacrypt/model/Game.java
+++ src/main/java/linguacrypt/model/Game.java
@@ -1,5 +1,19 @@
 package linguacrypt.model;
 
+import java.util.Random;
+
 public class Game {
-    
+    private GameConfiguration gConfig;
+    private Grid grid;
+    private int turn; // 0 : Blue team to play / 1 : Red team to play
+    Random random = new Random();
+
+    public Game(GameConfiguration gConfig, Grid grid) {
+        this.gConfig = gConfig;
+        this.grid = grid;
+    }
+
+    public void setUpGame(){
+        turn = random.nextInt(2);
+    }
 }
