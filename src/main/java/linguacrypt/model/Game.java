d911725736acc8629dea25255928cbda64899d11
Baptiste Pachot <baptiste.pachot@telecomnancy.eu>
2025-01-06 11:17:13 +0100

    feat : Init grid

--- src/main/java/linguacrypt/model/Game.java
+++ src/main/java/linguacrypt/model/Game.java
@@ -8,12 +8,32 @@ public class Game {
     private int turn; // 0 : Blue team to play / 1 : Red team to play
     Random random = new Random();
 
-    public Game(GameConfiguration gConfig, Grid grid) {
+    public Game(GameConfiguration gConfig) {
         this.gConfig = gConfig;
-        this.grid = grid;
+        this.grid = new Grid(gConfig.getGridSize());
+        setUpGame();
+        grid.printGrid();
     }
 
     public void setUpGame(){
         turn = random.nextInt(2);
+        grid.initGrid();
+    }
+
+    public GameConfiguration getgConfig() {
+        return gConfig;
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
     }
 }
--- src/main/java/linguacrypt/model/Grid.java
+++ src/main/java/linguacrypt/model/Grid.java
@@ -1,5 +1,7 @@
 package linguacrypt.model;
 
+import java.util.Random;
+
 public class Grid {
     private int size;
     private Card[][] grid;
@@ -7,6 +9,18 @@ public class Grid {
     public Grid(int size) {
         this.size = size;
         this.grid = new Card[size][size];
+    }
+
+    public void initGrid() {
+        BankCard bankCard = new BankCard();
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                String randomWord = bankCard.getWordsDefault().get(random.nextInt(bankCard.getWordsDefault().size()));
+                grid[i][j] = new Card(randomWord, "url_to_image");
+            }
+        }
     }
 
     public Card[][] getGrid(){
@@ -22,6 +36,15 @@ public class Grid {
     public void setCard(int row, int col, Card card) {
         if (row >= 0 && row < size && col >= 0 && col < size) {
             grid[row][col] = card;
+        }
+    }
+
+    public void printGrid() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j].getWord() + " ");
+            }
+            System.out.println();
         }
     }
 }
