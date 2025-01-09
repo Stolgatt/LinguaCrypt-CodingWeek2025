123a1860f76ff7c84d2ad21a5ea4263572a750f0
ROBERT Aymeric <aymeric.robert@telecomnancy.eu>
2025-01-09 23:04:35 +0100

    fix: temp timer in multi

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,7 +1,14 @@
 
-import Card;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import linguacrypt.model.game.Card;
 
 public class CardTest {
 
